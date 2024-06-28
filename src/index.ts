a734077abb1849eaffa323234251f0f68df07caa
Cat Chen <catchen@catchen.me>
2024-06-27 21:01:12 -0700

    Initial implementation

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,61 @@
-import { error, getState, info, saveState } from '@actions/core';
-import { context } from '@actions/github';
+import {
+  exportVariable,
+  getInput,
+  group,
+  notice,
+  setFailed,
+} from '@actions/core';
+import { getExecOutput } from '@actions/exec';
+import { tokenWhoAmI } from 'token-who-am-i';
 
-function run(): void {
-  if (!getState('isPost')) {
-    saveState('isPost', 'true');
-  }
-  info(`This is the Action context: ${JSON.stringify(context)}`);
-  error('Action needs to be implemented.');
+export async function configGitWithToken(githubToken: string): Promise<void> {
+  const me = await group('Run token-who-am-i', async () => {
+    return await tokenWhoAmI(githubToken);
+  });
+  notice(`Token login: ${me.login}`);
+
+  await group('Configure gh', async () => {
+    exportVariable('GH_TOKEN', githubToken);
+    await getExecOutput('gh', ['auth', 'status']);
+    await getExecOutput('gh', ['auth', 'setup-git']);
+  });
+
+  await group('Configure git', async () => {
+    await getExecOutput('git', ['config', '--list']);
+    await getExecOutput('git', [
+      'config',
+      '--global',
+      'user.email',
+      me.email ?? '',
+    ]);
+    await getExecOutput('git', [
+      'config',
+      '--global',
+      'user.name',
+      me.name ?? me.login,
+    ]);
+    const remoteOutput = await getExecOutput('git', [
+      'remote',
+      'get-url',
+      'origin',
+    ]);
+    const originUrl = remoteOutput.stdout.trim();
+    const originUrlWithToken = originUrl.replace(
+      /^https:\/\//,
+      `https://${me.login}:${githubToken}@`,
+    );
+    await getExecOutput('git', [
+      'remote',
+      'set-url',
+      'origin',
+      originUrlWithToken,
+    ]);
+  });
 }
 
-function cleanup(): void {
-  error('Post action needs to be implemented or removed.');
+async function run(): Promise<void> {
+  const githubToken = getInput('github-token');
+  await configGitWithToken(githubToken);
 }
 
-if (!getState('isPost')) {
-  run();
-} else {
-  cleanup();
-}
+run().catch((error: Error) => setFailed(error));
